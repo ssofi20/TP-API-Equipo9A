7e03e43761279711ae3472e53c606323f056dd5b
Sofia Bernigola <smbernigola@gmail.com>
2025-10-07 14:29:00 -0300

    Add initial ASP.NET MVC Web API project files

---
+++ CatalogoProductos/CatalogoProductos.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CatalogoProductos.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ CatalogoProductos/CatalogoProductos.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CatalogoProductos.API.WebApiApplication" Language="C#" %>
