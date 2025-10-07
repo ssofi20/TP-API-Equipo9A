d4fe7c218e3af2d61cb6ba0a4e71dfaae87dfe38
Sofia Bernigola <smbernigola@gmail.com>
2025-10-07 14:52:44 -0300

    Clarify action comments in ProductoController

--- CatalogoProductos/CatalogoProductos.API/Controllers/ProductoController.cs
+++ CatalogoProductos/CatalogoProductos.API/Controllers/ProductoController.cs
@@ -9,29 +9,29 @@ namespace CatalogoProductos.API.Controllers
 {
     public class ProductoController : ApiController
     {
-        // GET: api/Producto
+        // GET: api/Producto (Listar Productos)
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET: api/Producto/5
+        // GET: api/Producto/5 (Buscar Producto por Id)
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST: api/Producto
+        // POST: api/Producto (Dar de alta Producto)
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT: api/Producto/5
+        // PUT: api/Producto/5 (Modificar Producto)
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE: api/Producto/5
+        // DELETE: api/Producto/5 (Eliminar Producto)
         public void Delete(int id)
         {
         }
